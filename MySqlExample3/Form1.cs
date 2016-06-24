b4df8292dcb9030a0e5b3ca7dba3026a1bddd204
hiiwz <hiiwz@DESKTOP-5M1K6OM>
2016-06-24 23:17:05 +0800

    Add project files.

---
+++ MySqlExample3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlExample3;
+
+namespace MySqlExample3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'worldDataSet.country' table. You can move, or remove it, as needed.
+            //DataSet ds = new DataSet();
+            
+
+            this.countryTableAdapter.Fill(this.worldDataSet.country);
+            //server=localhost;user id=root;password=wang943698.com;persistsecurityinfo=True;database=world
+            MD5 md5 = new MD5();
+            this.textBox1.Text=md5.MD5Encrypt("server=localhost;user id=root;password=wang943698.com;persistsecurityinfo=True;database=world", "wsqwzy22");
+            
+        }
+    }
+}
